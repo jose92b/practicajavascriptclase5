f56769ec57940b8da20bb7e4d510f6a01f02e32c
Jose Bermudez <jose.92.b@hotmail.com>
2017-02-07 21:18:51 -0600

    Second commit

---
+++ Practicajavascript/Practicajavascript/JavaScript.js
@@ -0,0 +1,14 @@
+﻿function invertir() {
+    var cadena = document.getElementById('original').value;
+    var x = cadena.length;
+    var cadenaInvertida = "";
+    while (x >= 0) {
+        cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+        x--;
+    }
+    document.getElementById('Resultado').value = cadenaInvertida;
+}
+
+function charposition() {
+
+}
